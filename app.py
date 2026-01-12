2a86b3570001a2e93af58be3aea7d5fa9d47e4b8
gmkarshad45-design <gmkarshad45@gmail.com>
2026-01-12 21:49:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -310,6 +310,6 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    # Dynamic port for Render/Heroku deployment
+    # Dynamic port for Render deployment
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
